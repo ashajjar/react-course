35cd78d400c31515fc61800a2097b0ade351eb56
Ahmad Hajjar <ash852006@gmail.com>
2019-02-02 16:12:51 +0800

    Added styles and styles loaders to webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import IndecisionApp from './components/IndecisionApp';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/style.scss';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
